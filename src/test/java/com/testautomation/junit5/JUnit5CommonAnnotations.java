a44fcebfb3088e946791f3ecbd2d085cc3f4dd06
Sumon <sdtechie22@gmail.com>
2020-04-30 09:57:09 +0530

    Added JUnit Test Disabling feature

--- src/test/java/com/testautomation/junit5/JUnit5CommonAnnotations.java
+++ src/test/java/com/testautomation/junit5/JUnit5CommonAnnotations.java
@@ -1,12 +1,22 @@
 package com.testautomation.junit5;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 public class JUnit5CommonAnnotations {
+
+	// this gets executed once, before the start of all tests. It is used to perform
+	// time intensive activities e.g. to connect to a database. Methods marked
+	// with this annotation need to be defined as static to work with JUnit.
+	@BeforeAll
+	public static void beforeAll() {
+		System.out.println("Unit5 -> Inside beforeAll() method");
+	}
 
 	// this gets executed before each test. It is used to prepare the test
 	// environment e.g. reading input data, initializing the class etc.
@@ -38,6 +48,15 @@ public class JUnit5CommonAnnotations {
 	@AfterEach
 	public void afterEach() {
 		System.out.println("JUnit5 -> Inside afterEach() method");
+	}
+
+	// this gets executed once, after all tests have been finished. It is used to
+	// perform clean-up activities e.g. to disconnect from a database. Methods
+	// annotated with this annotation need to be defined as static to work with
+	// JUnit.
+	@AfterAll
+	public static void afterAll() {
+		System.out.println("Unit5 -> Inside afterAll() method");
 	}
 
 }
---
+++ src/test/java/com/testautomation/junit5/JUnit5DisablingTests.java
@@ -0,0 +1,60 @@
+package com.testautomation.junit5;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
+
+public class JUnit5DisablingTests {
+
+	/* The @Disabled annotation allows to statically ignore a test. */
+	@Disabled
+	public void disabledMethod() {
+		System.out.println("This method will not get executed.");
+	}
+
+	@Test
+	public void enabledMethod() {
+		System.out.println("This method will get executed.");
+	}
+
+	// Assumptions.assumeFalse marks the test as invalid, if its condition evaluates
+	// to true.
+	@Test
+	public void testOnDev1() {
+		System.setProperty("ENV", "DEV");
+		Assumptions.assumeFalse("DEV".equals(System.getProperty("ENV")), JUnit5DisablingTests::message);
+		// remainder of test will be aborted
+		System.out.println("This method will not get executed.");
+	}
+
+	@Test
+	public void testOnProd1() {
+		System.setProperty("ENV", "PROD");
+		Assumptions.assumeFalse("DEV".equals(System.getProperty("ENV")));
+		// remainder of test will proceed
+		System.out.println("This method will get executed.");
+	}
+
+	// Assumptions.assumeTrue evaluates the test as invalid if its condition
+	// evaluates to false.
+	@Test
+	public void testOnDev2() {
+		System.setProperty("ENV", "DEV");
+		Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
+		// remainder of test will proceed
+		System.out.println("This method will get executed.");
+	}
+
+	@Test
+	public void testOnProd2() {
+		System.setProperty("ENV", "PROD");
+		Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")), JUnit5DisablingTests::message);
+		// remainder of test will be aborted
+		System.out.println("This method will not get executed.");
+	}
+
+	private static String message() {
+		return "TEST Execution Failed :: ";
+	}
+
+}
